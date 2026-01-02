3ef280deb05ea966b48c141d3efd31faa8eca773
Kavina A C <kavinaac@gmail.com>
2026-01-02 15:29:19 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,21 +1,3 @@
-/*// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBgytxymbUI_VI5A8f3K6Qu9MGqS122h2E",
-  authDomain: "food-delivery-app-173ea.firebaseapp.com",
-  projectId: "food-delivery-app-173ea",
-  storageBucket: "food-delivery-app-173ea.firebasestorage.app",
-  messagingSenderId: "437560554676",
-  appId: "1:437560554676:web:75bcdc0edcdfbd058653e1"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-*/
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
